badeed76d3fda8f2bb83f616acbdab826c527b0d
hivank Goel <shivankgoel2@gmail.com>
2023-04-11 02:18:27 +0530

    Oauth completed

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require ("mongoose");
-const encrypt = require("mongoose-encryption")
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require("mongoose-findorcreate");
+
+
 
  const app = express()
 
@@ -12,20 +18,69 @@ const encrypt = require("mongoose-encryption")
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
 
+app.use(session({
+  secret: "our little Secret.",
+  resave:false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 mongoose.connect("mongodb://127.0.0.1/userDB");
 
 const userSchema = new mongoose.Schema( {
   email: String,
-  password:String
+  password:String,
+  googleId:String
 })
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields:['password']});
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = mongoose.model("User" , userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(function(user, cb) {
+  process.nextTick(function() {
+    cb(null, { id: user.id, username: user.username });
+  });
+});
+
+passport.deserializeUser(function(user, cb) {
+  process.nextTick(function() {
+    return cb(null, user);
+  });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.get("/", function(req, res){
   res.render("home");
 })
+
+app.get("/auth/google",
+passport.authenticate('google',{scope: ['profile']})
+);
+
+app.get("/auth/google/secrets",
+passport.authenticate("google", {failureRedirect: "/login"}),
+function(req,res){
+  res.redirect("/secrets");
+});
 
 app.get("/login", function(req, res){
   res.render("login");
@@ -35,30 +90,58 @@ app.get("/register", function(req, res){
   res.render("register");
 })
 
-app.post("/register", function(req, res){
-  const newUser = new User ({
-    email: req.body.username,
-    password: req.body.password
+app.get("/secrets",function(req,res){
+  if(req.isAuthenticated()){
+    res.render("secrets");
+  }else{
+    res.render("login");
+  }
+})
+
+app.get("/logout",function(req, res){
+  req.logout(function(err){
+    if(err){
+      console.log(err);
+    }else{
+        res.redirect("/");
+    }
   });
 
-  newUser.save(
-  ).then(res.render("secrets"));
+})
+
+app.post("/register", function(req, res){
+  User.register({username: req.body.username}, req.body.password, function(err,user){
+    if(err){
+      console.log(err);
+      res.render("/register")
+    }else{
+      passport.authenticate("local")(req, res,function(){
+        res.redirect("/secrets");
+      })
+    }
+  })
+
 
   });
 
 app.post("/login", function(req, res){
-  const username = req.body.username;
-  const password = req.body.password;
-  User.findOne({email: username}
-  ).then(function(foundUser){
-    if(foundUser.password === req.body.password){
-      res.render("secrets")
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  req.login(user, function(err){
+    if(err){
+      console.log(err);
+    }else{
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/secrets");
+      })
     }
-  }).catch(function(err){
-    console.log(err);
   })
 
-})
+
+});
 
 app.listen(3000, function(){
   console.log("Server running on Port 3000");
